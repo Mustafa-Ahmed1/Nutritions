d8105991e1f6cf2f7d00f3f27e2e74c219d43908
Mustafa Ahmed <mstfyjmalalden@gmail.com>
2022-07-12 23:27:23 +0300

    edit ui classes and styles

--- app/src/main/java/com/example/nutritionapp/ui/HomeActivity.kt
+++ app/src/main/java/com/example/nutritionapp/ui/HomeActivity.kt
@@ -1,32 +1,45 @@
 package com.example.nutritionapp.ui
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.util.Log
+import android.view.View
 import com.example.nutritionapp.CSVParser
 import com.example.nutritionapp.Calculation
 import com.example.nutritionapp.R
 import com.example.nutritionapp.data.DataManager
 import com.example.nutritionapp.data.model.Meal
+import com.example.nutritionapp.databinding.ActivityMainBinding
+import com.example.nutritionapp.ui.base.BaseActivity
 import com.example.nutritionapp.util.Constant
-import com.example.nutritionapp.util.ReadFiles
-import java.io.File
 import java.io.InputStreamReader
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : BaseActivity<ActivityMainBinding>() {
+
     val parser = CSVParser()
     private val calculation: Calculation = Calculation()
     lateinit var mealsList: MutableList<Meal>
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+
+    override fun bindingInflater() = ActivityMainBinding.inflate(layoutInflater)
+
+    override fun setUp() {
+        findViewById<View>(R.id.widget_app_action).visibility = View.GONE
+        binding.button.visibility = View.VISIBLE
+        navigate()
+
         openFile()
         mealsList = DataManager.getMeals()
     }
 
-    fun openFile(){
+    private fun navigate() {
+        binding.button.setOnClickListener {
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.add(R.id.root_fragment, homeFragment)
+            transaction.commit()
+        }
+    }
+
+    fun openFile() {
         val inputStream = assets.open(Constant.CSV_FILE_NAME)
         val buffer = InputStreamReader(inputStream)
         parser.getMealsFromCSV(buffer)
     }
 }
+
--- app/src/main/java/com/example/nutritionapp/ui/HomeFragment.kt
+++ app/src/main/java/com/example/nutritionapp/ui/HomeFragment.kt
@@ -7,17 +7,18 @@ import com.example.nutritionapp.R
 import com.example.nutritionapp.databinding.FragmentHomeBinding
 import com.example.nutritionapp.ui.base.BaseFragment
 
+
 class HomeFragment :BaseFragment<FragmentHomeBinding>() {
 
     override fun bindingInflater(): FragmentHomeBinding = FragmentHomeBinding.inflate(layoutInflater)
     override fun setUp() {
-        (activity as MainActivity).findViewById<Button>(R.id.button).visibility = View.GONE
-        (activity as MainActivity).findViewById<View>(R.id.widget_app_action).visibility = View.VISIBLE
-        (activity as MainActivity).findViewById<TextView>(R.id.app_action_title).text="Home Fragment"
+        (activity as HomeActivity).findViewById<Button>(R.id.button).visibility = View.GONE
+        (activity as HomeActivity).findViewById<View>(R.id.widget_app_action).visibility = View.VISIBLE
+        (activity as HomeActivity).findViewById<TextView>(R.id.app_action_title).text="Home Fragment"
 
-        (activity as MainActivity).findViewById<Button>(R.id.button_back).setOnClickListener{
-            (activity as MainActivity).findViewById<Button>(R.id.button).visibility = View.VISIBLE
-            (activity as MainActivity).findViewById<View>(R.id.widget_app_action).visibility = View.GONE
+        (activity as HomeActivity).findViewById<Button>(R.id.button_back).setOnClickListener{
+            (activity as HomeActivity).findViewById<Button>(R.id.button).visibility = View.VISIBLE
+            (activity as HomeActivity).findViewById<View>(R.id.widget_app_action).visibility = View.GONE
             val  transaction = fragmentManager?.beginTransaction()
             transaction?.remove(homeFragment)
             transaction?.commit()
