0232700338f6ab50f9eff4c5f90b8db0ca73be86
Mohammed Najm <mohammed.najm.422@gmail.com>
2022-07-14 11:29:13 +0300

    update quicksort function

--- app/src/main/java/com/example/nutritionapp/Calculation.kt
+++ app/src/main/java/com/example/nutritionapp/Calculation.kt
@@ -8,8 +8,8 @@ class Calculation {
 
     fun mealTop5(list:MutableList<Meal>,top:Int):List<Meal>{
 //        list.sortByDescending { (it.sugars*-1)+(it.potassium)+(it.carbohydrate*0.7)+(it.fiber*0.5)}
-
-       var l = quicksort (list)
+//        bloodPressure‏
+        var l = quicksort (list, "diabetics")
         Log.v("AMEER", "${l[0].name}")
         Log.v("AMEER", "${l[1].name}")
         Log.v("AMEER", "${l[2].name}")
@@ -22,22 +22,27 @@ class Calculation {
     }
 
     // Take Left (first) Index of the array and Right (last) Index of the array
-    fun quicksort(list:MutableList<Meal>):List<Meal>{
+    fun quicksort(list:MutableList<Meal>, diseaseName:String):List<Meal>{
         if (list.size < 2){
             return list
         }
+        lateinit var equal:List<Meal>
+        lateinit var less:List<Meal>
+        lateinit var greater:List<Meal>
 //      finding a pivot using (length of the list)/2 i.e middle element in the list
         val pivot = (list.size/2)
+        if(diseaseName == "diabetics" ){
 //      filter elements which are equal  the pivot and store it in a list called equal
-        val equal = list.filter { ((it.sugars*-1) + (it.potassium) + (it.carbohydrate*0.7) + (it.fiber*0.5)) == ((list[pivot].sugars*-1) + (list[pivot].potassium) + (list[pivot].carbohydrate*0.7) + (list[pivot].fiber*0.5))  }
+            equal = list.filter { ((it.sugars*-1) + (it.potassium) + (it.carbohydrate*0.7) + (it.fiber*0.5)) == ((list[pivot].sugars*-1) + (list[pivot].potassium) + (list[pivot].carbohydrate*0.7) + (list[pivot].fiber*0.5))  }
 //      filter elements which are lesser than the pivot and store it in a list called lesser
-        val less = list.filter { ((it.sugars*-1) + (it.potassium) + (it.carbohydrate*0.7) + (it.fiber*0.5)) > ((list[pivot].sugars*-1) + (list[pivot].potassium) + (list[pivot].carbohydrate*0.7) + (list[pivot].fiber*0.5)) }
+            less = list.filter { ((it.sugars*-1) + (it.potassium) + (it.carbohydrate*0.7) + (it.fiber*0.5)) > ((list[pivot].sugars*-1) + (list[pivot].potassium) + (list[pivot].carbohydrate*0.7) + (list[pivot].fiber*0.5)) }
 //      filter elements which are greater than the pivot and store it in a list called greater
-        val greater = list.filter { ((it.sugars*-1) + (it.potassium) + (it.carbohydrate*0.7) + (it.fiber*0.5)) < ((list[pivot].sugars*-1) + (list[pivot].potassium) + (list[pivot].carbohydrate*0.7) + (list[pivot].fiber*0.5)) }
+            greater = list.filter { ((it.sugars*-1) + (it.potassium) + (it.carbohydrate*0.7) + (it.fiber*0.5)) < ((list[pivot].sugars*-1) + (list[pivot].potassium) + (list[pivot].carbohydrate*0.7) + (list[pivot].fiber*0.5)) }
+        }
 //      call the function recursively to perform operation, but this time pass the splitted arrays.
 //      Do the same process again and again, till we left with multiple arrays with a single element and
 //      arranged in order
-        return quicksort(less as MutableList<Meal>) + equal + quicksort(greater as MutableList<Meal>)
+        return quicksort(less as MutableList<Meal>, diseaseName) + equal + quicksort(greater as MutableList<Meal>, diseaseName)
     }
 
 }
