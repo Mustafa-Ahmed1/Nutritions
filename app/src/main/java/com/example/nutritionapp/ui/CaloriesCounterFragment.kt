f283f41377a4199316aa0573a7da9d9fc2427af7
Mustafa Ahmed <mstfyjmalalden@gmail.com>
2022-07-14 08:21:10 +0300

    fix bugs in TestFragment.kt

--- app/src/main/java/com/example/nutritionapp/ui/CaloriesCounterFragment.kt
+++ app/src/main/java/com/example/nutritionapp/ui/CaloriesCounterFragment.kt
@@ -1,5 +1,6 @@
 package com.example.nutritionapp.ui
 
+import androidx.fragment.app.Fragment
 import com.example.nutritionapp.databinding.FragmentCounterCaloriesBinding
 import com.example.nutritionapp.ui.base.BaseFragment
 
@@ -13,6 +14,18 @@ class CaloriesCounterFragment : BaseFragment<FragmentCounterCaloriesBinding>() {
         binding.buttonBackSearchScreen.setOnClickListener {
             backNavigation(homeFragment)
         }
+    }
+
+    override var visibilityCustomActionBar: Boolean
+        get() = TODO("Not yet implemented")
+        set(value) {}
+
+    override fun title(): String? {
+        TODO("Not yet implemented")
+    }
+
+    override fun back(): Fragment? {
+        TODO("Not yet implemented")
     }
 
 }
--- app/src/main/java/com/example/nutritionapp/ui/TestFragment.kt
+++ app/src/main/java/com/example/nutritionapp/ui/TestFragment.kt
@@ -1,5 +1,6 @@
 package com.example.nutritionapp.ui
 
+import androidx.fragment.app.Fragment
 import com.example.nutritionapp.databinding.FragmentTestBinding
 import com.example.nutritionapp.ui.base.BaseFragment
 import com.example.nutritionapp.util.Constant
@@ -37,5 +38,12 @@ class TestFragment : BaseFragment<FragmentTestBinding>() {
             }
         }
     }
+
+    override var visibilityCustomActionBar: Boolean = true
+
+    override fun title(): String = arguments?.getString(Constant.KeyValues.MEAL_NAME).toString()
+
+    override fun back(): Fragment = MealsSearchFragment()
+
 
 }
