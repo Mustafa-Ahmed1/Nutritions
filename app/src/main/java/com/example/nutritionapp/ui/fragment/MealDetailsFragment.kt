e13007def140c7f4db2547818f6b9d7621fbab79
Salah-Kafaji <salah94master@gmail.com>
2022-07-19 21:11:41 +0300

    setup for Meal Details Fragment

--- app/src/main/java/com/example/nutritionapp/ui/fragment/MealDetailsFragment.kt
+++ app/src/main/java/com/example/nutritionapp/ui/fragment/MealDetailsFragment.kt
@@ -3,14 +3,15 @@ package com.example.nutritionapp.ui.fragment
 import android.util.Log
 import androidx.fragment.app.Fragment
 import com.example.nutritionapp.data.model.Meal
+import com.example.nutritionapp.databinding.FragmentMealDetailsBinding
 import com.example.nutritionapp.databinding.FragmentTestBinding
 import com.example.nutritionapp.ui.base.BaseFragment
 import com.example.nutritionapp.util.Constants
 
-class MealDetailsFragment : BaseFragment<FragmentTestBinding>() {
+class MealDetailsFragment : BaseFragment<FragmentMealDetailsBinding>() {
 
-    override fun bindingInflater(): FragmentTestBinding =
-        FragmentTestBinding.inflate(layoutInflater)
+    override fun bindingInflater(): FragmentMealDetailsBinding =
+        FragmentMealDetailsBinding.inflate(layoutInflater)
 
     override var visibilityCustomActionBar: Boolean = true
     override fun getTitle(): String = arguments?.getString(Constants.KeyValues.MEAL_NAME).toString()
@@ -23,14 +24,15 @@ class MealDetailsFragment : BaseFragment<FragmentTestBinding>() {
 
     private fun bindMeal(meal: Meal) {
         binding.apply {
-            caloriesValue.text = meal.calories.toString()
-            sugarsValue.text = meal.sugars.toString()
-            proteinsValue.text = meal.protein.toString()
-            fibersValue.text = meal.fiber.toString()
+//            caloriesValue.text = meal.calories.toString()
+//            sugarsValue.text = meal.sugars.toString()
+//            proteinsValue.text = meal.protein.toString()
+//            fibersValue.text = meal.fiber.toString()
         }
     }
 
     override fun onStart() {
         super.onStart()
     }
+
 }
